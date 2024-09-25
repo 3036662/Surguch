3c159c2d4401fb18357fcff1f36d11200686c64b
Oleg Proskurin <proskur@altlinux.org>
2024-09-25 17:06:27 +0300

    test signature find behavior

--- src/test/t_signature.cpp
+++ src/test/t_signature.cpp
@@ -1,5 +1,8 @@
 #include "t_signature.hpp"
+#include "models/pdf_page_model.hpp"
+#include "core/signature_processor.hpp"
 #include <QTest>
+#include <iostream>
 
 TSignature::TSignature(QObject *parent)
     : QObject{parent}
@@ -9,4 +12,56 @@ TSignature::TSignature(QObject *parent)
 
 void TSignature::testTrue(){
      QVERIFY(true);
+}
+
+void TSignature::testModelConstruct(){
+    PdfPageModel model;
+    model.setSource(file1_);
+    QVERIFY(model.rowCount({})==14);
+    QVERIFY(model.getCtx()!=nullptr);
+    QVERIFY(model.getDoc()!=nullptr);
+    QVERIFY(model.getPdfDoc()!=nullptr);
+}
+
+
+void TSignature::createProcessorFail(){
+    QVERIFY_THROWS_EXCEPTION(std::runtime_error,core::SignatureProcessor(nullptr,nullptr));
+}
+
+void TSignature::createProcessor(){
+    PdfPageModel model;
+    model.setSource(file1_);
+    std::unique_ptr<core::SignatureProcessor> processor=nullptr;
+    QVERIFY_THROWS_NO_EXCEPTION(processor=std::make_unique<core::SignatureProcessor>(model.getCtx(),model.getPdfDoc()));
+    QVERIFY(processor);
+}
+
+
+void TSignature::findSignatures(){
+    PdfPageModel model;
+    model.setSource(file1_);
+    std::unique_ptr<core::SignatureProcessor> processor=nullptr;
+    QVERIFY_THROWS_NO_EXCEPTION(processor=std::make_unique<core::SignatureProcessor>(model.getCtx(),model.getPdfDoc()));
+    QVERIFY(processor);
+    QVERIFY(processor->findAcroForm()!=nullptr);
+    QVERIFY(processor->findSignatures());
+    std::bitset<32> sig_flags=processor->getFormSigFlags();
+    QVERIFY2(sig_flags.test(0),"SignatureExist flag is not set");
+    QVERIFY2(sig_flags.test(1),"AppendOnly flag is not set");
+    QVERIFY2(sig_flags.to_ulong()==3 || sig_flags.to_ulong()==1,"Unknown SigFlag is set");
+    QVERIFY(processor->getSignaturesCount()==1);
+}
+
+
+void TSignature::signaturesNumber(){
+    QVERIFY(sigNumb(file1_,1));
+    QVERIFY(sigNumb(file2_,1));
+    QVERIFY(sigNumb(file3_,5));
+}
+
+bool TSignature::sigNumb(const QString& file,int sig_expected) const{
+    PdfPageModel model;
+    model.setSource(file);
+    core::SignatureProcessor prc(model.getCtx(),model.getPdfDoc());
+    return prc.findSignatures() && (prc.getSignaturesCount()==sig_expected);
 }
--- src/test/t_signature.hpp
+++ src/test/t_signature.hpp
@@ -15,6 +15,24 @@ private slots:
 
     void testTrue();
 
+    void testModelConstruct();
+
+    void createProcessorFail();
+
+    void createProcessor();
+
+    void findSignatures();
+
+    void signaturesNumber();
+
+private:
+
+    bool sigNumb(const QString& file,int sig_expected) const;
+
+    const QString test_files_dir_= TEST_FILES_DIR;
+    const QString file1_=test_files_dir_+"13_cam_CADES-XLT1_1sig.pdf";
+    const QString file2_=test_files_dir_+"14_acrob_CADES-XLT1.pdf";
+    const QString file3_=test_files_dir_+"21_cam_CADES-XLT1_5signs.pdf";
 };
 
 #endif // T_SIGNATURE_HPP
